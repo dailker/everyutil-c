9dd49dcf30c6d369ca81a71faad50cf34121ddbb
dailker <ilkeroztbm@gmail.com>
2024-07-30 19:22:51 -0400

    Add include/array/array_cumulative_product.h

---
+++ include/array/array_cumulative_product.h
@@ -0,0 +1,34 @@
+#ifndef ARRAY_ARRAY_CUMULATIVE_PRODUCT_H
+#define ARRAY_ARRAY_CUMULATIVE_PRODUCT_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if defined(_WIN32) || defined(_WIN64)
+  #ifdef BUILDING_EVERYUTIL
+    #define EVERYUTIL_API __declspec(dllexport)
+  #else
+    #define EVERYUTIL_API __declspec(dllimport)
+  #endif
+#else
+  #define EVERYUTIL_API
+#endif
+
+#include <stddef.h>
+
+/**
+ * Returns the cumulative product of a double array.
+ *
+ * @param array Pointer to input array of doubles.
+ * @param length Number of elements in the input array.
+ * @return Pointer to a newly allocated array containing cumulative products.
+ *         Returns NULL on failure. Caller must free the returned array.
+ */
+EVERYUTIL_API double* array_cumulative_product(const double* array, size_t length);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // ARRAY_ARRAY_CUMULATIVE_PRODUCT_H
