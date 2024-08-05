c8a961afa875ffc8b5c24bee3ce521d8fee24f09
dailker <ilkeroztbm@gmail.com>
2024-08-05 07:39:34 -0400

    Remove deleted files

--- fractal_free.c
+++
@@ -1,3 +0,0 @@
-#include "array/fractal_chunks.h"
-
-// Implementation is in fractal_chunks.c. Do not define here to avoid multiple definition errors."
--- semantic_group_free.c
+++
@@ -1,3 +0,0 @@
-#include "array/semantic_group.h"
-
-// Implementation is in semantic_group.c. Do not define here to avoid multiple definition errors.
