a1cfa5168d0aa37b6a59e11cdab6832f472195a5
dailker <ilkeroztbm@gmail.com>
2024-08-17 13:14:13 -0400

    Add tests/array/test_array_symmetric_difference.c

---
+++ tests/array/test_array_symmetric_difference.c
@@ -0,0 +1,4 @@
+// Stub test for array_symmetric_difference
+#include <stdio.h>
+#include "array/array_symmetric_difference.h"
+int main(void) { printf("test_array_symmetric_difference: STUB\n"); return 0; }
