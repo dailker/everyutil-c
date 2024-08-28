2e0dcd725f371c80f8b7a2189b49afdea96694f6
dailker <ilkeroztbm@gmail.com>
2024-08-28 16:16:46 -0400

    Add include/color/pixelate_color.h

---
+++ include/color/pixelate_color.h
@@ -0,0 +1,21 @@
+#ifndef PIXELATE_COLOR_H
+#define PIXELATE_COLOR_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include <stddef.h>
+#include "color/parse_color_string.h"
+#if defined(_WIN32) || defined(__CYGWIN__)
+  #ifdef EVERYUTIL_BUILD
+    #define EVERYUTIL_API __declspec(dllexport)
+  #else
+    #define EVERYUTIL_API __declspec(dllimport)
+  #endif
+#else
+  #define EVERYUTIL_API
+#endif
+EVERYUTIL_API char *pixelate_color(const char *color, int pixel_size);
+#ifdef __cplusplus
+}
+#endif
+#endif
