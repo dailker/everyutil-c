ae4e353117ed6ffe1f3738d48401df642ca2442a
dailker <ilkeroztbm@gmail.com>
2024-09-01 15:55:17 -0400

    Add include/logic/any_pass.h

---
+++ include/logic/any_pass.h
@@ -0,0 +1,28 @@
+#ifndef LOGIC_ANY_PASS_H
+#define LOGIC_ANY_PASS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if defined(_WIN32) || defined(_WIN64)
+  #ifdef EVERYUTIL_BUILD
+    #define EVERYUTIL_API __declspec(dllexport)
+  #else
+    #define EVERYUTIL_API __declspec(dllimport)
+  #endif
+#else
+  #define EVERYUTIL_API
+#endif
+
+#include <stddef.h>
+
+typedef int (*predicate_func)(int);
+
+EVERYUTIL_API int any_pass(int value, predicate_func* predicates, size_t count);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // LOGIC_ANY_PASS_H
