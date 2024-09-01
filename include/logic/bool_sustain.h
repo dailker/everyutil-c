e94e11f6c3f9db7dd449b9d861d96ac958f0c5eb
dailker <ilkeroztbm@gmail.com>
2024-09-01 19:34:16 -0400

    Add include/logic/bool_sustain.h

---
+++ include/logic/bool_sustain.h
@@ -0,0 +1,28 @@
+#ifndef LOGIC_BOOL_SUSTAIN_H
+#define LOGIC_BOOL_SUSTAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if defined(_WIN32) || defined(_WIN64)
+  #ifdef EVERYUTIL_BUILD
+    #define EVERYUTIL_API __declspec(dllexport)
+  #else
+    #define EVERYUTIL_API __declspec(dllimport)
+  #endif
+#else
+  #define EVERYUTIL_API
+#endif
+
+#include <stddef.h>
+#include <stdlib.h>
+typedef int (*predicate_func)(int);
+
+EVERYUTIL_API int bool_sustain(int input, int duration);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
