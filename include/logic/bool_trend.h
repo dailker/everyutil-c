4d07fa0249bb354c80fdfd5a528ea93ba9f174e9
dailker <ilkeroztbm@gmail.com>
2024-09-01 10:33:10 -0400

    Add include/logic/bool_trend.h

---
+++ include/logic/bool_trend.h
@@ -0,0 +1,28 @@
+#ifndef LOGIC_BOOL_TREND_H
+#define LOGIC_BOOL_TREND_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if defined(_WIN32) || defined(_WIN64)
+  #ifdef EVERYUTIL_BUILD
+    #define EVERYUTIL_API __declspec(dllexport)
+  #else
+    #define EVERYUTIL_API __declspec(dllimport)
+  #endif
+#else
+  #define EVERYUTIL_API
+#endif
+
+#include <stddef.h>
+typedef int (*predicate_func)(int);
+typedef int (*transform_func)(int);
+
+EVERYUTIL_API int bool_trend(const int* values, size_t size);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
