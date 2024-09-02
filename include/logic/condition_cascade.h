ca542b0b84f294d727584a7de4fbcf295cc3831e
dailker <ilkeroztbm@gmail.com>
2024-09-02 15:20:29 -0400

    Add include/logic/condition_cascade.h

---
+++ include/logic/condition_cascade.h
@@ -0,0 +1,28 @@
+#ifndef LOGIC_CONDITION_CASCADE_H
+#define LOGIC_CONDITION_CASCADE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if defined(_WIN32) || defined(_WIN64)
+  #ifdef EVERYUTIL_BUILD
+    #define EVERYUTIL_API __declspec(dllexport)
+  #else
+    #define EVERYUTIL_API __declspec(dllimport)
+  #endif
+#else
+  #define EVERYUTIL_API
+#endif
+
+#include <stddef.h>
+typedef int (*predicate_func)(int);
+typedef int (*transform_func)(int);
+
+EVERYUTIL_API int condition_cascade(int value, predicate_func* conditions, int* results, size_t count);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
