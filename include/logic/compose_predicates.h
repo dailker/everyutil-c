1efa17e55e8007e46c659a2e66dc8bcc9309278b
dailker <ilkeroztbm@gmail.com>
2024-09-02 15:11:45 -0400

    Add include/logic/compose_predicates.h

---
+++ include/logic/compose_predicates.h
@@ -0,0 +1,28 @@
+#ifndef LOGIC_COMPOSE_PREDICATES_H
+#define LOGIC_COMPOSE_PREDICATES_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if defined(_WIN32) || defined(_WIN64)
+  #ifdef EVERYUTIL_BUILD
+    #define EVERYUTIL_API __declspec(dllexport)
+  #else
+    #define EVERYUTIL_API __declspec(dllimport)
+  #endif
+#else
+  #define EVERYUTIL_API
+#endif
+
+#include <stddef.h>
+typedef int (*predicate_func)(int);
+typedef int (*transform_func)(int);
+
+EVERYUTIL_API int compose_predicates(int value, predicate_func pred1, predicate_func pred2);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
