fb311b54594f617b78ed1afcaeabf4b73daa3d05
dailker <ilkeroztbm@gmail.com>
2024-09-02 13:50:41 -0400

    Add include/logic/branch.h

---
+++ include/logic/branch.h
@@ -0,0 +1,28 @@
+#ifndef LOGIC_BRANCH_H
+#define LOGIC_BRANCH_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if defined(_WIN32) || defined(_WIN64)
+  #ifdef EVERYUTIL_BUILD
+    #define EVERYUTIL_API __declspec(dllexport)
+  #else
+    #define EVERYUTIL_API __declspec(dllimport)
+  #endif
+#else
+  #define EVERYUTIL_API
+#endif
+
+#include <stddef.h>
+
+typedef int (*predicate_func)(int);
+
+EVERYUTIL_API int branch(int condition, int true_val, int false_val);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // LOGIC_BRANCH_H
