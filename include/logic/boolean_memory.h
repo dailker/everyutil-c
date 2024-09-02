01d3b7f336c67cec5979eb8abefa4ef2f278aafe
dailker <ilkeroztbm@gmail.com>
2024-09-02 17:15:06 -0400

    Add include/logic/boolean_memory.h

---
+++ include/logic/boolean_memory.h
@@ -0,0 +1,28 @@
+#ifndef LOGIC_BOOLEAN_MEMORY_H
+#define LOGIC_BOOLEAN_MEMORY_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if defined(_WIN32) || defined(_WIN64)
+  #ifdef EVERYUTIL_BUILD
+    #define EVERYUTIL_API __declspec(dllexport)
+  #else
+    #define EVERYUTIL_API __declspec(dllimport)
+  #endif
+#else
+  #define EVERYUTIL_API
+#endif
+
+#include <stddef.h>
+#include <stdlib.h>
+typedef int (*predicate_func)(int);
+
+EVERYUTIL_API int boolean_memory(int input);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
