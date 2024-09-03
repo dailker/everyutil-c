5195df2b05bd9a245dbadb5a8f051e2974644b61
dailker <ilkeroztbm@gmail.com>
2024-09-03 16:13:15 -0400

    Add include/logic/create_boolean_timer.h

---
+++ include/logic/create_boolean_timer.h
@@ -0,0 +1,28 @@
+#ifndef LOGIC_CREATE_BOOLEAN_TIMER_H
+#define LOGIC_CREATE_BOOLEAN_TIMER_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if defined(_WIN32) || defined(_WIN64)
+  #ifdef EVERYUTIL_BUILD
+    #define EVERYUTIL_API __declspec(dllexport)
+  #else
+    #define EVERYUTIL_API __declspec(dllimport)
+  #endif
+#else
+  #define EVERYUTIL_API
+#endif
+
+#include <stddef.h>
+typedef int (*predicate_func)(int);
+typedef int (*transform_func)(int);
+
+EVERYUTIL_API int create_boolean_timer(int input, int duration);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
