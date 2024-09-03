b7ba5336b22c50df59559c7ec9440b0300598d72
dailker <ilkeroztbm@gmail.com>
2024-09-03 10:52:12 -0400

    Add include/logic/decide_by_priority.h

---
+++ include/logic/decide_by_priority.h
@@ -0,0 +1,28 @@
+#ifndef LOGIC_DECIDE_BY_PRIORITY_H
+#define LOGIC_DECIDE_BY_PRIORITY_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if defined(_WIN32) || defined(_WIN64)
+  #ifdef EVERYUTIL_BUILD
+    #define EVERYUTIL_API __declspec(dllexport)
+  #else
+    #define EVERYUTIL_API __declspec(dllimport)
+  #endif
+#else
+  #define EVERYUTIL_API
+#endif
+
+#include <stddef.h>
+#include <stdlib.h>
+typedef int (*predicate_func)(int);
+
+EVERYUTIL_API int decide_by_priority(const int* values, const int* priorities, size_t count);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
