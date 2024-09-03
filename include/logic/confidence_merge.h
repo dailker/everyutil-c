2ee93455d4ebfc4a404ac713fdab119269047f0b
dailker <ilkeroztbm@gmail.com>
2024-09-03 19:51:55 -0400

    Add include/logic/confidence_merge.h

---
+++ include/logic/confidence_merge.h
@@ -0,0 +1,28 @@
+#ifndef LOGIC_CONFIDENCE_MERGE_H
+#define LOGIC_CONFIDENCE_MERGE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if defined(_WIN32) || defined(_WIN64)
+  #ifdef EVERYUTIL_BUILD
+    #define EVERYUTIL_API __declspec(dllexport)
+  #else
+    #define EVERYUTIL_API __declspec(dllimport)
+  #endif
+#else
+  #define EVERYUTIL_API
+#endif
+
+#include <stddef.h>
+typedef int (*predicate_func)(int);
+typedef int (*transform_func)(int);
+
+EVERYUTIL_API int confidence_merge(double conf1, double conf2);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
