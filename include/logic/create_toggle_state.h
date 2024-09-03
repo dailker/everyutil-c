38d39192c29622993fbd7e5a03ceee64e144a377
dailker <ilkeroztbm@gmail.com>
2024-09-03 13:21:13 -0400

    Add include/logic/create_toggle_state.h

---
+++ include/logic/create_toggle_state.h
@@ -0,0 +1,28 @@
+#ifndef LOGIC_CREATE_TOGGLE_STATE_H
+#define LOGIC_CREATE_TOGGLE_STATE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if defined(_WIN32) || defined(_WIN64)
+  #ifdef EVERYUTIL_BUILD
+    #define EVERYUTIL_API __declspec(dllexport)
+  #else
+    #define EVERYUTIL_API __declspec(dllimport)
+  #endif
+#else
+  #define EVERYUTIL_API
+#endif
+
+#include <stddef.h>
+#include <stdlib.h>
+typedef int (*predicate_func)(int);
+
+EVERYUTIL_API int create_toggle_state(int trigger);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
