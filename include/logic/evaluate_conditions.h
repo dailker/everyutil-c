8ebd93ad5a8edf0cf1c2c464ab69336744bc915e
dailker <ilkeroztbm@gmail.com>
2024-09-03 15:11:37 -0400

    Add include/logic/evaluate_conditions.h

---
+++ include/logic/evaluate_conditions.h
@@ -0,0 +1,28 @@
+#ifndef LOGIC_EVALUATE_CONDITIONS_H
+#define LOGIC_EVALUATE_CONDITIONS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if defined(_WIN32) || defined(_WIN64)
+  #ifdef EVERYUTIL_BUILD
+    #define EVERYUTIL_API __declspec(dllexport)
+  #else
+    #define EVERYUTIL_API __declspec(dllimport)
+  #endif
+#else
+  #define EVERYUTIL_API
+#endif
+
+#include <stddef.h>
+#include <stdlib.h>
+typedef int (*predicate_func)(int);
+
+EVERYUTIL_API int evaluate_conditions(const int* conditions, size_t count, int require_all);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
