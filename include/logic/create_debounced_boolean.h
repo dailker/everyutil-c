6e7d6f147bb973ce5fa9449b89fef2a6da0c6868
dailker <ilkeroztbm@gmail.com>
2024-09-03 20:39:55 -0400

    Add include/logic/create_debounced_boolean.h

---
+++ include/logic/create_debounced_boolean.h
@@ -0,0 +1,28 @@
+#ifndef LOGIC_CREATE_DEBOUNCED_BOOLEAN_H
+#define LOGIC_CREATE_DEBOUNCED_BOOLEAN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if defined(_WIN32) || defined(_WIN64)
+  #ifdef EVERYUTIL_BUILD
+    #define EVERYUTIL_API __declspec(dllexport)
+  #else
+    #define EVERYUTIL_API __declspec(dllimport)
+  #endif
+#else
+  #define EVERYUTIL_API
+#endif
+
+#include <stddef.h>
+typedef int (*predicate_func)(int);
+typedef int (*transform_func)(int);
+
+EVERYUTIL_API int create_debounced_boolean(int input, int threshold);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
