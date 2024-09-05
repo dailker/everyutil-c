648c419d530fb38612e802fa12c5848a84d46aba
dailker <ilkeroztbm@gmail.com>
2024-09-04 21:58:33 -0400

    Add include/logic/group_by_predicate.h

---
+++ include/logic/group_by_predicate.h
@@ -0,0 +1,28 @@
+#ifndef LOGIC_GROUP_BY_PREDICATE_H
+#define LOGIC_GROUP_BY_PREDICATE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if defined(_WIN32) || defined(_WIN64)
+  #ifdef EVERYUTIL_BUILD
+    #define EVERYUTIL_API __declspec(dllexport)
+  #else
+    #define EVERYUTIL_API __declspec(dllimport)
+  #endif
+#else
+  #define EVERYUTIL_API
+#endif
+
+#include <stddef.h>
+#include <stdlib.h>
+typedef int (*predicate_func)(int);
+
+EVERYUTIL_API int group_by_predicate(const int* values, size_t count, predicate_func predicate, int* true_group, int* false_group);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
