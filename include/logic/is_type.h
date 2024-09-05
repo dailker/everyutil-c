d561f5854a0f9dd05e95e0209b687c7021501bb7
dailker <ilkeroztbm@gmail.com>
2024-09-05 08:34:07 -0400

    Add include/logic/is_type.h

---
+++ include/logic/is_type.h
@@ -0,0 +1,27 @@
+#ifndef LOGIC_IS_TYPE_H
+#define LOGIC_IS_TYPE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if defined(_WIN32) || defined(_WIN64)
+  #ifdef EVERYUTIL_BUILD
+    #define EVERYUTIL_API __declspec(dllexport)
+  #else
+    #define EVERYUTIL_API __declspec(dllimport)
+  #endif
+#else
+  #define EVERYUTIL_API
+#endif
+
+#include <stddef.h>
+typedef int (*predicate_func)(int);
+
+EVERYUTIL_API int is_type(int value, int type_id);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
