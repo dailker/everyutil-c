d13f46b6d5b8d76bcfcb319a4a42db687c6671cc
dailker <ilkeroztbm@gmail.com>
2024-09-05 10:02:47 -0400

    Add include/logic/logic_tree_to_string.h

---
+++ include/logic/logic_tree_to_string.h
@@ -0,0 +1,28 @@
+#ifndef LOGIC_LOGIC_TREE_TO_STRING_H
+#define LOGIC_LOGIC_TREE_TO_STRING_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if defined(_WIN32) || defined(_WIN64)
+  #ifdef EVERYUTIL_BUILD
+    #define EVERYUTIL_API __declspec(dllexport)
+  #else
+    #define EVERYUTIL_API __declspec(dllimport)
+  #endif
+#else
+  #define EVERYUTIL_API
+#endif
+
+#include <stddef.h>
+typedef int (*predicate_func)(int);
+typedef int (*transform_func)(int);
+
+EVERYUTIL_API int logic_tree_to_string(int input);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
