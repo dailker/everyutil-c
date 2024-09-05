a1df4ddccf8bc39172471ff8f143f07de399bcc3
dailker <ilkeroztbm@gmail.com>
2024-09-05 15:51:25 -0400

    Add include/logic/is_nil.h

---
+++ include/logic/is_nil.h
@@ -0,0 +1,24 @@
+#ifndef LOGIC_IS_NIL_H
+#define LOGIC_IS_NIL_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if defined(_WIN32) || defined(_WIN64)
+  #ifdef EVERYUTIL_BUILD
+    #define EVERYUTIL_API __declspec(dllexport)
+  #else
+    #define EVERYUTIL_API __declspec(dllimport)
+  #endif
+#else
+  #define EVERYUTIL_API
+#endif
+
+EVERYUTIL_API int is_nil(void* val);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // LOGIC_IS_NIL_H
