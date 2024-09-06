926bcecb925ed6096ea1e3f2bb482675303b119f
dailker <ilkeroztbm@gmail.com>
2024-09-05 22:15:41 -0400

    Add include/logic/match_pattern.h

---
+++ include/logic/match_pattern.h
@@ -0,0 +1,28 @@
+#ifndef LOGIC_MATCH_PATTERN_H
+#define LOGIC_MATCH_PATTERN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if defined(_WIN32) || defined(_WIN64)
+  #ifdef EVERYUTIL_BUILD
+    #define EVERYUTIL_API __declspec(dllexport)
+  #else
+    #define EVERYUTIL_API __declspec(dllimport)
+  #endif
+#else
+  #define EVERYUTIL_API
+#endif
+
+#include <stddef.h>
+#include <stdlib.h>
+typedef int (*predicate_func)(int);
+
+EVERYUTIL_API int match_pattern(const int* input, const int* pattern, size_t size);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
