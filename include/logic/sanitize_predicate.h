ea9529af8e43e09698e32fe69a193c1f3c651588
dailker <ilkeroztbm@gmail.com>
2024-09-07 17:34:58 -0400

    Add include/logic/sanitize_predicate.h

---
+++ include/logic/sanitize_predicate.h
@@ -0,0 +1,27 @@
+#ifndef LOGIC_SANITIZE_PREDICATE_H
+#define LOGIC_SANITIZE_PREDICATE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if defined(_WIN32) || defined(_WIN64)
+  #ifdef EVERYUTIL_BUILD
+    #define EVERYUTIL_API __declspec(dllexport)
+  #else
+    #define EVERYUTIL_API __declspec(dllimport)
+  #endif
+#else
+  #define EVERYUTIL_API
+#endif
+
+#include <stddef.h>
+typedef int (*predicate_func)(int);
+
+EVERYUTIL_API int sanitize_predicate(int value);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
