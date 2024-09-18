419e9c9132a1bb9b6314bc8290e3777348d8429e
dailker <ilkeroztbm@gmail.com>
2024-09-18 12:51:26 -0400

    Add src/logic/count_truthy.c

---
+++ src/logic/count_truthy.c
@@ -0,0 +1,11 @@
+#include "logic/count_truthy.h"
+
+EVERYUTIL_API int count_truthy(const int* values, size_t size) {
+    if (!values) return 0;
+    
+    int count = 0;
+    for (size_t i = 0; i < size; i++) {
+        if (values[i]) count++;
+    }
+    return count;
+}
